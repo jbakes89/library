25e967840cc32553b128988bba81e489228f0232
Joseph <joe.bakes89@googlemail.com>
2021-06-01 02:00:42 +0900

    Multiple changes to visual appearance and book editing function. Implemented the ability to add new books.

--- README.md
+++ README.md
@@ -12,19 +12,26 @@ This was created as part of The Odin Project's Full Stack JavaScript curriculum.
 - (FUTURE GOAL) Storing data in localStorage
 
 ### Things to add/improve
+- Following the class/object definition method being taught in this lesson was constricting. I wanted to create more custom objects (e.g., for the add/edit book menu), but resources online for e.g., inheritance from HTML element classes almost all used ES6 Classes. If I were to remake this app, I would probably make custom `EditBookMenu` and `AddBookMenu` classes, and I'd also like to separate things out into modules for better readability.
+- An option for cloud storage
+- Visually, the app would look much better with covers. Obviously I can't host an up-to-date archive of book covers for the sake of this practice project. It looks like it would be possible to implement something using the [Open Library API](https://openlibrary.org/developers/api), but I don't know how fast it will be. Once a cover has been retrieved, would it be possible to cache the cover in localStorage? - This isn't something I've had a chance to explore yet.
 
-### Patch notes (29th May, 2021)
-- Setup a functioning edit menu that allows books to be added to the library
-- Setup edit and delete functions for existing books
+### Patch notes (1st June, 2021)
+- Implemented read status for the bookcase display and the edit menu.
+- Implemented scrolling for the bookcase and set the edit menu position to update with scrolling.
+- Implemented function for adding new books.
+- Resolved some issues with adding/editing function.
+- Prevented editing individual books from refreshing the whole bookcase.
+- Changed edit menu to show different text depending on whether adding a new book or editing an existing book.
+- Started to make some visual adjustments.
 
-### TODO (29th May, 2021)
-- Bind the checkboxes on each book with the `hasBeenRead` property
+### TODO (1st June, 2021)
 - Build upon edit function
-    - Add a button for adding new books
-    - Build 'has been read' parameter into the edit function
+    - Inactivate submit button (and show visually) for invalid input and before changes when editing.
 - Add localStorage function
-- Workout how to handle long text
+- Work out how to handle long text
     - flexible font size?
     - limit max length?
     - ellipsis or other suitable overflow method?
 - Other improvements to appearance
+    - Dynamically adjust the number of books per row to stay above a certain min width when the vieport width changes.
--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const testButton = document.querySelector(".js-test");
-testButton.addEventListener("click", () => (toggleEditMenu()));
-
 /* Global variables */
 let myLibrary = [];
 
 // HTML elements
 const editMenu = document.querySelector(".js-edit-menu");
 const bookcase = document.querySelector(".js-bookcase");
+const bookshelvesSpace = document.querySelector(".js-bookshelves-space");
 const editForm = document.querySelector(".js-edit-form");
+const editSubmitButton = document.querySelector(".js-edit-submit-button");
 const editCancelButton = document.querySelector(".js-edit-cancel-button");
+const addBookButton = document.querySelector(".js-add-book-button");
 
 // Flags
-let inEditMode = false;
 let bookBeingEdited = null;
 
 /* Book Object Definition */
@@ -33,11 +32,12 @@ Book.prototype.asDiv = function () {
     const book = template.cloneNode(true);
     book.classList.remove("js-book-template");
     const map = {
-        "{js-read}": `--${this.hasBeenRead ? "read" : "unread"}`,
+        "{js-read-class}": `${this.hasBeenRead ? " --js-read" : ""}`,
         "{js-title}": this.title,
         "{js-author}": this.author,
         "{js-pages}": String(this.numOfPages),
-        "{js-index}": myLibrary.indexOf(this)
+        "{js-index}": myLibrary.indexOf(this),
+        "{js-read-checkbox}": this.hasBeenRead ? " checked" : ""
     }
     let filterStrings = "";
     for (var prop in map) {
@@ -48,24 +48,37 @@ Book.prototype.asDiv = function () {
     return book;
 }
 
+Book.prototype.setReadStatus = function (readStatus) {
+    this.hasBeenRead = readStatus;
+    const bookIndex = myLibrary.indexOf(this);
+    const bookElement = document.querySelector(`.js-book[data-index-number="${bookIndex}"]`);
+    readStatus ? bookElement.classList.add("--js-read") : bookElement.classList.remove("--js-read");
+    bookElement.querySelector(".js-book-read-checkbox").checked = readStatus;
+}
+
+Book.prototype.toggleReadStatus = function () {
+    this.setReadStatus(!this.hasBeenRead);
+}
+
 /* Core Functions */
 // Adding books
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    refreshBookDisplay();
+    addToDisplayWithBooks(book);
 }
 
 function addBooksToLibrary(books) {
     for (book of books) {
         myLibrary.push(book);
     }
-    refreshBookDisplay();
+    addToDisplayWithBooks(books);
 }
+
+addBookButton.addEventListener("click", openAddMenu);
 
 // Editing books
 function editBook(book) {
-    fillEditFormWith(book.title, book.author, book.numOfPages);
-    inEditMode = true;
+    fillEditFormWith(book.title, book.author, book.numOfPages, book.hasBeenRead);
     bookBeingEdited = book;
     openEditMenu();
 }
@@ -81,12 +94,12 @@ const editButtonPressed = function (e){
 // Deleting books
 function deleteBook(book) {
     myLibrary.splice(myLibrary.indexOf(book), 1);
-    refreshBookDisplay();
+    refreshWholeBookDisplay();
 }
 
 function deleteBookAtIndex(index) {
     myLibrary.splice(index, 1);
-    refreshBookDisplay();
+    refreshWholeBookDisplay();
 }
 
 const deleteButtonPressed = function (e){
@@ -97,36 +110,97 @@ const deleteButtonPressed = function (e){
     }
 }
 
-// View
-function refreshBookDisplay() {
+// Changing read status
+const readCheckboxToggled = function (e){
+    const bookElement = e.target.closest(".js-book");
+    const bookIndex = parseInt(bookElement.dataset.indexNumber);
+    if (bookIndex < myLibrary.length) {
+        myLibrary[bookIndex].setReadStatus(e.target.checked);
+    }
+}
+
+
+/* Updating view */
+// Update/refresh books on display
+function addToDisplayWithBooks(books) {
+    const parsedArray = (books instanceof Book) ? [books] : books
     const fragment = document.createDocumentFragment();
-    for (var book of myLibrary) {
+    for (var book of parsedArray) {
         const bookElement = book.asDiv();
         bindEventsToBookElement(bookElement);
         fragment.appendChild(bookElement);
     }
-    const booksWrapper = document.querySelector(".js-bookshelves-space");
-    booksWrapper.innerHTML = "";
-    booksWrapper.appendChild(fragment);
+    bookshelvesSpace.appendChild(fragment);    
+}
+
+function refreshWholeBookDisplay() {
+    const addBookNode = bookshelvesSpace.firstElementChild;
+    bookshelvesSpace.innerHTML = "";
+    bookshelvesSpace.appendChild(addBookNode);
+    addToDisplayWithBooks(myLibrary);
+}
+
+function updateBook(book, newTitle, newAuthor, newPages, newReadStatus) {
+    const bookIndex = myLibrary.indexOf(book);
+    if (bookIndex == -1) {
+        addBookToLibrary(new Book(newTitle, newAuthor, newPages, newReadStatus));
+    } else {
+        const bookElement = bookshelvesSpace.querySelector(`.js-book[data-index-number="${bookIndex}"]`);
+        bookElement.querySelector(".js-book-title").textContent = book.title = newTitle;
+        bookElement.querySelector(".js-book-author").textContent = book.author = newAuthor;
+        bookElement.querySelector(".js-book-pages").textContent = book.numOfPages = String(newPages) + "p";
+        book.setReadStatus(newReadStatus);
+    }
 }
 
 
-/* Edit Menu */
-// Opening/closing
-function openEditMenu() {
+/* Edit/Add Menu */
+// Opening
+function openAddMenu() {
+    editForm.reset();
+    openEditMenu(true);
+}
+
+function openEditMenu(inAddMode) {
+    if (inAddMode) {
+        editSubmitButton.textContent = "+";
+        editSubmitButton.classList.add("--js-add-mode");
+    } else {
+        editSubmitButton.textContent = "\u2713";
+        editSubmitButton.classList.remove("--js-add-mode");
+        // Disable submit button if no changes have been made
+        editSubmitButton.disabled = true;
+    }
+
     // Make sure editMenu is visible
     editMenu.style.display = "block";
     editMenu.offsetWidth;
 
     editMenu.classList.remove("--js-hidden");
-    bookcase.classList.add("--js-background");
+    bookshelvesSpace.classList.add("--js-background");
 }
 
+// Check for changes on input
+function bookDetailsHaveBeenEdited() {
+    return !(bookBeingEdited.title == editForm.elements["edit-title"].value &&
+        bookBeingEdited.author == editForm.elements["edit-author"].value &&
+        bookBeingEdited.numOfPages == parseInt(editForm.elements["edit-pages"].value) &&
+        bookBeingEdited.hasBeenRead == editForm.elements["edit-read-status"].checked
+    )
+}
+
+editForm.addEventListener("input", e => {
+    editSubmitButton.disabled = !bookDetailsHaveBeenEdited();
+})
+
+// Closing
 function closeEditMenu() {
     editMenu.classList.add("--js-hidden");
-    bookcase.classList.remove("--js-background");
+    bookshelvesSpace.classList.remove("--js-background");
+    bookBeingEdited = null;
 }
 
+// Toggle open/closed
 function toggleEditMenu() {
     if (editMenu.classList.contains("--js-hidden")) {
         openEditMenu();
@@ -135,56 +209,65 @@ function toggleEditMenu() {
     }
 }
 
+// Actions after transition
 editMenu.addEventListener("transitionend", () => {
     if (editMenu.classList.contains("--js-hidden")) {
-        // Actions after closing
         editMenu.style.display = "none";
         editForm.reset();
+    } else {
+        editForm.elements[0].focus();
     }
-})
+});
+
+// Close with mouse click outside of edit menu
+document.addEventListener("mousedown", e => {
+    if (!editMenu.classList.contains("--js-hidden") && // Edit menu is open
+        e.target != editMenu && // and click outside edit menu
+        !editMenu.contains(e.target) // and its children...
+    ) {
+        closeEditMenu();
+    }
+});
+
+
+// Update top of editMenu when the bookcase is scrolled
+bookcase.addEventListener("scroll", e => {
+    editMenu.style.top = `${bookcase.scrollTop}px`;
+});
+
 
 // Submit
-const submitForm = function(e) {
-    if (e.target == editForm) {
-        // Prevent default
-        e.preventDefault();
-        if (inEditMode) {
-            bookBeingEdited.title = e.target.elements["edit-title"].value;
-            bookBeingEdited.author = e.target.elements["edit-author"].value;
-            bookBeingEdited.pages = parseInt(e.target.elements["edit-pages"].value);
-            refreshBookDisplay();
-            inEditMode = false;
-            bookBeingEdited = null;
-        } else {
-            // Create book from input data
-            const book = new Book(
-                e.target.elements["edit-title"].value,
-                e.target.elements["edit-author"].value,
-                parseInt(e.target.elements["edit-pages"].value),
-                false
-            );
-            // Add book to library
-            addBookToLibrary(book);
-        }
+function submitEditForm() {
+    if (editForm.reportValidity()) {
+        updateBook(bookBeingEdited,
+            editForm.elements["edit-title"].value,
+            editForm.elements["edit-author"].value,
+            parseInt(editForm.elements["edit-pages"].value),
+            editForm.elements["edit-read-status"].checked);
+        bookBeingEdited = null;
 
         // Close form
         closeEditMenu();
     }
 }
-editForm.addEventListener("submit", submitForm)
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitEditForm();
+});
 
 
 // Cancel
-const cancelForm = function(e) {
+const cancelEditForm = function(e) {
     closeEditMenu();
 }
-editCancelButton.addEventListener("click", cancelForm);
+editCancelButton.addEventListener("click", cancelEditForm);
 
 // Pre-filling
-function fillEditFormWith(title, author, pages) {
+function fillEditFormWith(title, author, pages, readStatus) {
     editForm.elements["edit-title"].value = title;
     editForm.elements["edit-author"].value = author;
     editForm.elements["edit-pages"].value = parseInt(pages);
+    editForm.elements["edit-read-status"].checked = readStatus;
 }
 
 
@@ -195,28 +278,32 @@ function bindEventsToBookElement(bookElement) {
 
     const deleteButton = bookElement.querySelector(".js-book-delete-button");
     deleteButton.addEventListener("click", deleteButtonPressed);
+
+    const readCheckbox = bookElement.querySelector(".js-book-read-checkbox");
+    readCheckbox.addEventListener("click", readCheckboxToggled);
 }
 
 /* Keyboard Interaction */
 const onKeydown = function(e) {
-    switch (e.code) {
+    console.log(`Keydown event with code ${e.key}`)
+    switch (e.key) {
         // Handle "Enter" presses in book editing form
         case "Enter":
         case "NumpadEnter":
             if (e.target.form == editForm) {
+                console.log(`activeElement is ${document.activeElement}`);
                 e.preventDefault();
-                // Cycle focus
+                submitEditForm();
             }
             break;
         // Cancel form on "Escape" presses
         case "Escape":
         case "Esc":
-            cancelForm();
+            cancelEditForm();
             break;
-    }
-    if ((e.code == "Enter" || e.code == "NumpadEnter") && e.target.form == editForm) {
-        e.preventDefault();
-        // Cycle focus
+        case "NumpadAdd":
+        case "+":
+            openAddMenu();
     }
 }
 document.addEventListener("keydown", onKeydown);
