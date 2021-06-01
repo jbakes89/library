83e085e2869f590d5e231dd5e54d400b57cb1a9c
Joseph <joe.bakes89@googlemail.com>
2021-06-02 04:23:25 +0900

    Implemented local storage. Cleaned up several visual aspects.

--- README.md
+++ README.md
@@ -9,29 +9,27 @@ This was created as part of The Odin Project's Full Stack JavaScript curriculum.
 - Making objects (/classes) as functions
 - Adding methods to the object's prototype
 - Having an offscreen menu that can be opened and closed
+- Customising checkbox appearance
 - (FUTURE GOAL) Storing data in localStorage
 
 ### Things to add/improve
 - Following the class/object definition method being taught in this lesson was constricting. I wanted to create more custom objects (e.g., for the add/edit book menu), but resources online for e.g., inheritance from HTML element classes almost all used ES6 Classes. If I were to remake this app, I would probably make custom `EditBookMenu` and `AddBookMenu` classes, and I'd also like to separate things out into modules for better readability.
 - An option for cloud storage
 - Visually, the app would look much better with covers. Obviously I can't host an up-to-date archive of book covers for the sake of this practice project. It looks like it would be possible to implement something using the [Open Library API](https://openlibrary.org/developers/api), but I don't know how fast it will be. Once a cover has been retrieved, would it be possible to cache the cover in localStorage? - This isn't something I've had a chance to explore yet.
+- Undo function to revert last change
 
-### Patch notes (1st June, 2021)
-- Implemented read status for the bookcase display and the edit menu.
-- Implemented scrolling for the bookcase and set the edit menu position to update with scrolling.
-- Implemented function for adding new books.
-- Resolved some issues with adding/editing function.
-- Prevented editing individual books from refreshing the whole bookcase.
-- Changed edit menu to show different text depending on whether adding a new book or editing an existing book.
-- Started to make some visual adjustments.
+### Patch notes (2nd June, 2021)
+- Cleaned up the appearance of the buttons on the books, including a custom checkbox/toggle button.
+- Implemented local storage.
+- Added a `confirm` box before deleting a book.
+- Implemented responsive grid sizing and font sizes on the book covers.
 
-### TODO (1st June, 2021)
-- Build upon edit function
+### TODO (2nd June, 2021)
+- Improve edit menu
     - Inactivate submit button (and show visually) for invalid input and before changes when editing.
-- Add localStorage function
-- Work out how to handle long text
-    - flexible font size?
-    - limit max length?
-    - ellipsis or other suitable overflow method?
-- Other improvements to appearance
-    - Dynamically adjust the number of books per row to stay above a certain min width when the vieport width changes.
+    - Figure out how to prevent the edit menu being squashed when the window height is too small
+        - Setting `overflow-y: scroll` is okay, but the double scrollbar doesn't look nice. I know I can hide the scrollbar 
+
+## External Resources
+### Icons
+Some of the icons used on this site were taken from Google's Material Icons (available [here](https://fonts.google.com/icons) at the time of writing). Specifically, these were "/assets/delete_icon.svg", "/assets/edit_icon.svg", and "/assets/read_icon.svg". These icons are available for use under the [Apache License Version 2.0](https://www.apache.org/licenses/LICENSE-2.0.txt).
--- main.js
+++ main.js
@@ -12,13 +12,15 @@ const addBookButton = document.querySelector(".js-add-book-button");
 
 // Flags
 let bookBeingEdited = null;
+let useLocalStorage = false;
 
 /* Book Object Definition */
 function Book(title, author, numOfPages, hasBeenRead) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.hasBeenRead = hasBeenRead;
+    this.title = title || "";
+    this.author = author || "";
+    this.numOfPages = numOfPages || 1;
+    this.hasBeenRead = hasBeenRead || false;
+    this.storageKey = null;
 }
 
 Book.prototype.info = function () {
@@ -54,11 +56,36 @@ Book.prototype.setReadStatus = function (readStatus) {
     const bookElement = document.querySelector(`.js-book[data-index-number="${bookIndex}"]`);
     readStatus ? bookElement.classList.add("--js-read") : bookElement.classList.remove("--js-read");
     bookElement.querySelector(".js-book-read-checkbox").checked = readStatus;
+    this.saveToStorage();
 }
 
 Book.prototype.toggleReadStatus = function () {
     this.setReadStatus(!this.hasBeenRead);
 }
+
+Book.prototype.toJSONString = function () {
+    return JSON.stringify(this);
+}
+
+Book.prototype.saveToStorage = function () {
+    saveBookToStorage(this);
+}
+
+Book.prototype.delete = function () {
+    const bookIndex = myLibrary.indexOf(this);
+    myLibrary.splice(bookIndex, 1);
+    this.deleteFromStorage();
+}
+
+Book.prototype.deleteFromStorage = function () {
+    deleteBookFromStorage(this);
+}
+
+Book.fromString = function (JSONString) {
+    const parsedString = JSON.parse(JSONString);
+    return Object.assign(new Book(), parsedString);
+}
+
 
 /* Core Functions */
 // Adding books
@@ -93,12 +120,12 @@ const editButtonPressed = function (e){
 
 // Deleting books
 function deleteBook(book) {
-    myLibrary.splice(myLibrary.indexOf(book), 1);
+    book.delete();
     refreshWholeBookDisplay();
 }
 
 function deleteBookAtIndex(index) {
-    myLibrary.splice(index, 1);
+    myLibrary[index].delete();
     refreshWholeBookDisplay();
 }
 
@@ -106,7 +133,8 @@ const deleteButtonPressed = function (e){
     const bookElement = e.target.closest(".js-book");
     const bookIndex = parseInt(bookElement.dataset.indexNumber);
     if (bookIndex < myLibrary.length) {
-        deleteBookAtIndex(bookIndex);
+        const book = myLibrary[bookIndex];
+        if (confirm(`Are you sure you want to delete ${book.title}?`)) { deleteBook(book); }
     }
 }
 
@@ -120,7 +148,7 @@ const readCheckboxToggled = function (e){
 }
 
 
-/* Updating view */
+/* Updating bookcase view */
 // Update/refresh books on display
 function addToDisplayWithBooks(books) {
     const parsedArray = (books instanceof Book) ? [books] : books
@@ -143,15 +171,84 @@ function refreshWholeBookDisplay() {
 function updateBook(book, newTitle, newAuthor, newPages, newReadStatus) {
     const bookIndex = myLibrary.indexOf(book);
     if (bookIndex == -1) {
-        addBookToLibrary(new Book(newTitle, newAuthor, newPages, newReadStatus));
+        // If book is not found in myLibrary, add a new book
+        const newBook = new Book(newTitle, newAuthor, newPages, newReadStatus)
+        addBookToLibrary(newBook);
+        newBook.saveToStorage();
     } else {
-        const bookElement = bookshelvesSpace.querySelector(`.js-book[data-index-number="${bookIndex}"]`);
+        // Otherwise, edit existing book
+        const bookElement = getBookElementWithindex(bookIndex);
         bookElement.querySelector(".js-book-title").textContent = book.title = newTitle;
         bookElement.querySelector(".js-book-author").textContent = book.author = newAuthor;
-        bookElement.querySelector(".js-book-pages").textContent = book.numOfPages = String(newPages) + "p";
+        book.numOfPages = newPages;
+        bookElement.querySelector(".js-book-pages").textContent = String(book.numOfPages) + "p";
         book.setReadStatus(newReadStatus);
+        book.saveToStorage();
     }
 }
+
+function getBookElementWithIndex(bookIndex) {
+    return bookshelvesSpace.querySelector(`.js-book[data-index-number="${bookIndex}"]`)
+}
+
+// Responsive resizing
+function getCurrentNumberOfColumns() {
+    const computedStyle = window.getComputedStyle(bookshelvesSpace);
+    return computedStyle.gridTemplateColumns.split(" ").length;
+}
+
+function getCurrentNumberOfRows() {
+    const computedStyle = window.getComputedStyle(bookshelvesSpace);
+    return computedStyle.gridTemplateRows.split(" ").length;
+}
+
+function changeNumberOfColumnsBy(n) {
+    const delta = parseInt(n);
+    if (delta) {
+        const currentNumColumns = getCurrentNumberOfColumns();
+        bookshelvesSpace.style.gridTemplateColumns = `repeat(${currentNumColumns + delta}, 1fr)`;
+    }
+}
+
+function elementOverflows(el) {
+    return el.scrollHeight > el.clientHeight || el.scrollWidth > el.clientWidth;
+}
+
+const MIN_BOOK_WIDTH = "145px";
+const DEFAULT_BOOK_WIDTH = "180px"
+
+const windowWasResized = function () {
+    /* Adjusting number of columns depending on book size */
+    // Check that there are any books in the display
+    const sampleBook = bookshelvesSpace.querySelector(".js-book")
+    if (!sampleBook) { return; };
+    // Check book width
+    const bookWidth = sampleBook.offsetWidth;
+    if (sampleBook.offsetWidth < parseFloat(MIN_BOOK_WIDTH)) {
+        // If smaller than min-width, reduce number of columns
+        changeNumberOfColumnsBy(-1);
+    } else {
+        // Increase the number of columns if the books will fit at default width
+        const numColumns = getCurrentNumberOfColumns();
+        const proposedWidth = parseFloat(DEFAULT_BOOK_WIDTH) * (numColumns + 1);
+        const currentOccupiedWidth = bookWidth * numColumns;
+        if (proposedWidth <= currentOccupiedWidth &&
+            getCurrentNumberOfRows() > 1
+        ) {
+            changeNumberOfColumnsBy(1);
+        }
+    }
+
+    /* Adjusting font size to fit on book*/
+    // Baseline font size is set to fit decently at the MIN_BOOK_WIDTH, so we scale proportionally with the width
+    const widthRatio = bookWidth/parseFloat(MIN_BOOK_WIDTH);
+    const rootStyle = window.getComputedStyle(document.body)
+    const baselineFontSize = rootStyle.getPropertyValue("--baseline-book-font-size");
+    const newFontSize = `${parseFloat(baselineFontSize) * widthRatio}px`;
+    document.documentElement.style.setProperty("--js-book-font-size", newFontSize);
+};
+
+window.addEventListener("resize", windowWasResized);
 
 
 /* Edit/Add Menu */
@@ -180,13 +277,17 @@ function openEditMenu(inAddMode) {
     bookshelvesSpace.classList.add("--js-background");
 }
 
-// Check for changes on input
+// Disable submit button when editing if nothing has been changed
 function bookDetailsHaveBeenEdited() {
-    return !(bookBeingEdited.title == editForm.elements["edit-title"].value &&
-        bookBeingEdited.author == editForm.elements["edit-author"].value &&
-        bookBeingEdited.numOfPages == parseInt(editForm.elements["edit-pages"].value) &&
-        bookBeingEdited.hasBeenRead == editForm.elements["edit-read-status"].checked
-    )
+    if (bookBeingEdited) {
+        return !(bookBeingEdited.title == editForm.elements["edit-title"].value &&
+            bookBeingEdited.author == editForm.elements["edit-author"].value &&
+            bookBeingEdited.numOfPages == parseInt(editForm.elements["edit-pages"].value) &&
+            bookBeingEdited.hasBeenRead == editForm.elements["edit-read-status"].checked
+        )
+    } else {
+        return true;
+    }
 }
 
 editForm.addEventListener("input", e => {
@@ -308,11 +409,104 @@ const onKeydown = function(e) {
 }
 document.addEventListener("keydown", onKeydown);
 
-// BOOK SAMPLE FOR TESTING
-addBooksToLibrary([
-    new Book("The Midnight Library", "Matt Haig", 288, false),
-    new Book("Klara and the Sun", "Kazuo Ishiguro", 304, true),
-    new Book("To Kill a Mockingbird", "Harper Lee", 324, false),
-    new Book("The Little Prince", "Antoine de Saint-Exup\u00E9ry", 93, true),
-    new Book("Harry Potter and the Deathly Hallows", "J.K. Rowling", 759, false)
-]);
+/* Local storage */
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        let x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // Everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // Test name field too
+            // Everything except Firefox
+            e.name === "QoutaExceededError" ||
+            // Firefox
+            e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            // Acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable("localStorage")) {
+    console.log("localStorage is available");
+    useLocalStorage = true;
+} else {
+    useLocalStorage = false;
+    alert("Please note that the book collection will not be saved.\n" +
+        "If you would like to save your collection, please check your local storage settings.");
+}
+
+function loadBooksFromStorage() {
+    if (useLocalStorage) {
+        const storage = window.localStorage;
+        Object.keys(storage).forEach( (bookKey) => {
+            const fetchedBookString = storage.getItem(bookKey)
+            const fetchedBook = Book.fromString(fetchedBookString);
+            myLibrary.push(fetchedBook);
+            addToDisplayWithBooks(fetchedBook);
+        })
+    }
+}
+
+function saveBookToStorage(book) {
+    if (useLocalStorage) {
+        const storage = window.localStorage;
+        const key = book.storageKey ?? function() {
+            const currentKeys = Object.keys(storage);
+            let highestKey = -1;
+            if (currentKeys.length > 0) {
+                highestKey = Object.keys(storage).reduce((a,b) => {
+                    return Math.max(a,b);
+                });
+            }
+            const newKey = `${parseInt(highestKey) + 1}`
+            book.storageKey = newKey;
+            return newKey
+        }();
+        storage.setItem(key, book.toJSONString())
+    }
+}
+
+function deleteBookFromStorage(book) {
+    if (useLocalStorage) {
+        const storage = window.localStorage;
+        if (Object.keys(storage).includes(book.storageKey)) {
+            storage.removeItem(book.storageKey);
+        }
+    }
+}
+
+function clearStorage() {
+    if (useLocalStorage) {
+        window.localStorage.clear();
+    }
+}
+
+// Load books from storage, or fill library with placeholder books if local storage is empty
+if (useLocalStorage) {
+    if (window.localStorage.length > 0) {
+        loadBooksFromStorage();
+    } else {
+        const placeholderBooks = [
+            new Book("The Midnight Library", "Matt Haig", 288, false),
+            new Book("Klara and the Sun", "Kazuo Ishiguro", 304, true),
+            new Book("To Kill a Mockingbird", "Harper Lee", 324, false),
+            new Book("The Little Prince", "Antoine de Saint-Exup\u00E9ry", 93, true),
+            new Book("Harry Potter and the Deathly Hallows", "J.K. Rowling", 759, false)
+        ];
+        addBooksToLibrary(placeholderBooks);
+        for (book of placeholderBooks) {
+            book.saveToStorage();
+        }
+    }
+}
+
+windowWasResized();
